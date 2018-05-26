659682bddd84fa49805735c3ecbe288cf81ce141
Zachary Fu <zachfu97@gmail.com>
2018-05-26 13:49:20 -0700

    Updated Arduino and Processing codes

--- LSM9DS1_I2C_TEST/LSM9DS1_I2C_TEST.ino
+++ LSM9DS1_I2C_TEST/LSM9DS1_I2C_TEST.ino
@@ -1,54 +1,14 @@
 /*****************************************************************
 LSM9DS1_Basic_I2C.ino
-SFE_LSM9DS1 Library Simple Example Code - I2C Interface
-Jim Lindblom @ SparkFun Electronics
-Original Creation Date: April 30, 2015
-https://github.com/sparkfun/LSM9DS1_Breakout
 
-The LSM9DS1 is a versatile 9DOF sensor. It has a built-in
-accelerometer, gyroscope, and magnetometer. Very cool! Plus it
-functions over either SPI or I2C.
-
-This Arduino sketch is a demo of the simple side of the
-SFE_LSM9DS1 library. It'll demo the following:
-* How to create a LSM9DS1 object, using a constructor (global
-  variables section).
-* How to use the begin() function of the LSM9DS1 class.
-* How to read the gyroscope, accelerometer, and magnetometer
-  using the readGryo(), readAccel(), readMag() functions and 
-  the gx, gy, gz, ax, ay, az, mx, my, and mz variables.
-* How to calculate actual acceleration, rotation speed, 
-  magnetic field strength using the calcAccel(), calcGyro() 
-  and calcMag() functions.
-* How to use the data from the LSM9DS1 to calculate 
-  orientation and heading.
-
-Hardware setup: This library supports communicating with the
-LSM9DS1 over either I2C or SPI. This example demonstrates how
-to use I2C. The pin-out is as follows:
-	LSM9DS1 --------- Arduino
-	 SCL ---------- SCL (A5 on older 'Duinos')
-	 SDA ---------- SDA (A4 on older 'Duinos')
+Connection for both sensors to Arduino:
+	LSM9DS1 -------- Arduino
+	 SCL ------------- SCL (A5 on older 'Duinos')
+	 SDA ------------- SDA (A4 on older 'Duinos')
 	 VDD ------------- 3.3V
 	 GND ------------- GND
-(CSG, CSXM, SDOG, and SDOXM should all be pulled high. 
-Jumpers on the breakout board will do this for you.)
 
-The LSM9DS1 has a maximum voltage of 3.6V. Make sure you power it
-off the 3.3V rail! I2C pins are open-drain, so you'll be 
-(mostly) safe connecting the LSM9DS1's SCL and SDA pins 
-directly to the Arduino.
-
-Development environment specifics:
-	IDE: Arduino 1.6.3
-	Hardware Platform: SparkFun Redboard
-	LSM9DS1 Breakout Version: 1.0
-
-This code is beerware. If you see me (or any other SparkFun 
-employee) at the local, and you've found our code helpful, 
-please buy us a round!
-
-Distributed as-is; no warranty is given.
+SDO_M and SDO_AG must be pulled down to GND on one of the sensors
 *****************************************************************/
 // The SFE_LSM9DS1 library requires both Wire and SPI be
 // included BEFORE including the 9DS1 library.
@@ -59,54 +19,48 @@ Distributed as-is; no warranty is given.
 //////////////////////////
 // LSM9DS1 Library Init //
 //////////////////////////
-// Use the LSM9DS1 class to create an object. [imu] can be
-// named anything, we'll refer to that throught the sketch.
-LSM9DS1 imu;
-LSM9DS1 imu2;
+
+LSM9DS1 sensor_front;
+LSM9DS1 sensor_back;
 
 ///////////////////////
 // Example I2C Setup //
 ///////////////////////
 // SDO_XM and SDO_G are both pulled high, so our addresses are:
-#define LSM9DS1_M1	0x1E // Would be 0x1C if SDO_M is LOW
-#define LSM9DS1_AG1	0x6B // Would be 0x6A if SDO_AG is LOW
-#define LSM9DS1_M2  0x1C // Would be 0x1C if SDO_M is LOW
-#define LSM9DS1_AG2  0x6A // Would be 0x6A if SDO_AG is LOW
+#define MAG_FRONT	0x1E 
+#define ACC_FRONT	0x6B 
+#define MAG_BACK  0x1C // SDO_M pulled down
+#define ACC_BACK  0x6A // SDO_AG pulled down
 
 ////////////////////////////
 // Sketch Output Settings //
 ////////////////////////////
-#define PRINT_CALCULATED
-//#define PRINT_RAW
-#define PRINT_SPEED 250 // 250 ms between prints
+#define PRINT_SPEED 50 // ms between prints
 static unsigned long lastPrint = 0; // Keep track of print time
 
-// Earth's magnetic field varies by location. Add or subtract 
-// a declination to get a more accurate heading. Calculate 
-// your's here:
-// http://www.ngdc.noaa.gov/geomag-web/#declination
-#define DECLINATION -8.58 // Declination (degrees) in Boulder, CO.
+//http://www.magnetic-declination.com/
+#define DECLINATION 16.23 // Declination (degrees) in Vancouver, Canada (16 deg, 14')
 
 void setup() 
 {
   
   Serial.begin(115200);
   
-  // Before initializing the IMU, there are a few settings
+  // Before initializing the sensor_front, there are a few settings
   // we may need to adjust. Use the settings struct to set
   // the device's communication mode and addresses:
-  imu.settings.device.commInterface = IMU_MODE_I2C;
-  imu.settings.device.mAddress = LSM9DS1_M1;
-  imu.settings.device.agAddress = LSM9DS1_AG1;
-  imu2.settings.device.commInterface = IMU_MODE_I2C;
-  imu2.settings.device.mAddress = LSM9DS1_M2;
-  imu2.settings.device.agAddress = LSM9DS1_AG2;
+  sensor_front.settings.device.commInterface = IMU_MODE_I2C;
+  sensor_front.settings.device.mAddress = MAG_FRONT;
+  sensor_front.settings.device.agAddress = ACC_FRONT;
+  sensor_back.settings.device.commInterface = IMU_MODE_I2C;
+  sensor_back.settings.device.mAddress = MAG_BACK;
+  sensor_back.settings.device.agAddress = ACC_BACK;
   // The above lines will only take effect AFTER calling
-  // imu.begin(), which verifies communication with the IMU
+  // sensor_front.begin(), which verifies communication with the sensor_front
   // and turns it on.
-  if (!imu.begin())
+  if (!sensor_front.begin())
   {
-    Serial.println("Failed to communicate with LSM9DS1_1.");
+    Serial.println("Failed to communicate with front sensor.");
     Serial.println("Double-check wiring.");
     Serial.println("Default settings in this sketch will " \
                   "work for an out of the box LSM9DS1 " \
@@ -115,9 +69,9 @@ void setup()
     while (1)
       ;
   }
-  if (!imu2.begin())
+  if (!sensor_back.begin())
   {
-    Serial.println("Failed to communicate with LSM9DS1_2.");
+    Serial.println("Failed to communicate with back sensor.");
     Serial.println("Double-check wiring.");
     Serial.println("Default settings in this sketch will " \
                   "work for an out of the box LSM9DS1 " \
@@ -130,145 +84,60 @@ void setup()
 
 void loop()
 {
-  // Update the sensor values whenever new data is available
-  if ( imu.gyroAvailable() )
+  if ( sensor_front.accelAvailable() )
   {
-    // To read from the gyroscope,  first call the
-    // readGyro() function. When it exits, it'll update the
-    // gx, gy, and gz variables with the most current data.
-    imu.readGyro();
-    imu2.readGyro();
+    sensor_front.readAccel();
   }
-  if ( imu.accelAvailable() )
+  if ( sensor_back.accelAvailable() )
   {
-    // To read from the accelerometer, first call the
-    // readAccel() function. When it exits, it'll update the
-    // ax, ay, and az variables with the most current data.
-    imu.readAccel();
-    imu2.readAccel();
+    sensor_back.readAccel();
   }
-  if ( imu.magAvailable() )
+  if ( sensor_front.magAvailable() )
   {
-    // To read from the magnetometer, first call the
-    // readMag() function. When it exits, it'll update the
-    // mx, my, and mz variables with the most current data.
-    imu.readMag();
-    imu2.readMag();
+    sensor_front.readMag();
+  }
+  if ( sensor_back.magAvailable() )
+  {
+    sensor_back.readMag();
   }
   
   if ((lastPrint + PRINT_SPEED) < millis())
   {
-    //printGyro();  // Print "G: gx, gy, gz"
-    printAccel(); // Print "A: ax, ay, az"
-    //printAttitude(imu.ax, imu.ay, imu.az, 
-  //               -imu.my, -imu.mx, imu.mz);
-                 
-    //printAttitude(imu2.ax, imu2.ay, imu2.az, 
-//                 -imu2.my, -imu2.mx, imu2.mz);
+    printAccel();
+    Serial.print(", ");
+    printAttitude(sensor_front);
+    Serial.print(", ");
+    printAttitude(sensor_back);
     Serial.println();
     
-    lastPrint = millis(); // Update lastPrint time
+    lastPrint = millis();
   }
-}
-
-void printGyro()
-{
-  // Now we can use the gx, gy, and gz variables as we please.
-  // Either print them as raw ADC values, or calculated in DPS.
-  Serial.print("G: ");
-#ifdef PRINT_CALCULATED
-  // If you want to print calculated values, you can use the
-  // calcGyro helper function to convert a raw ADC value to
-  // DPS. Give the function the value that you want to convert.
-  Serial.print(imu.calcGyro(imu.gx), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcGyro(imu.gy), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcGyro(imu.gz), 2);
-  Serial.print(", ");
-  Serial.print(imu2.calcGyro(imu2.gx), 2);
-  Serial.print(", ");
-  Serial.print(imu2.calcGyro(imu2.gy), 2);
-  Serial.print(", ");
-  Serial.print(imu2.calcGyro(imu2.gz), 2);
-  Serial.println(" deg/s");
-#elif defined PRINT_RAW
-  Serial.print(imu.gx);
-  Serial.print(", ");
-  Serial.print(imu.gy);
-  Serial.print(", ");
-  Serial.println(imu.gz);
-#endif
 }
 
 void printAccel()
 {  
-  // Now we can use the ax, ay, and az variables as we please.
-  // Either print them as raw ADC values, or calculated in g's.
-  //Serial.print("A: ");
-#ifdef PRINT_CALCULATED
-  // If you want to print calculated values, you can use the
-  // calcAccel helper function to convert a raw ADC value to
-  // g's. Give the function the value that you want to convert.
-  Serial.print(imu.calcAccel(imu.ax), 2);
+  Serial.print(sensor_front.calcAccel(sensor_front.ax), 3);
   Serial.print(", ");
-  Serial.print(imu.calcAccel(imu.ay), 2);
+  Serial.print(sensor_front.calcAccel(sensor_front.ay), 3);
   Serial.print(", ");
-  Serial.print(imu.calcAccel(imu.az), 2);
+  Serial.print(sensor_front.calcAccel(sensor_front.az), 3);
   Serial.print(", ");
-  Serial.print(imu2.calcAccel(imu2.ax), 2);
+  Serial.print(sensor_back.calcAccel(sensor_back.ax), 3);
   Serial.print(", ");
-  Serial.print(imu2.calcAccel(imu2.ay), 2);
+  Serial.print(sensor_back.calcAccel(sensor_back.ay), 3);
   Serial.print(", ");
-  Serial.print(imu2.calcAccel(imu2.az), 2);
-  //Serial.println(" g");
-#elif defined PRINT_RAW 
-  Serial.print(imu.ax);
-  Serial.print(", ");
-  Serial.print(imu.ay);
-  Serial.print(", ");
-  Serial.println(imu.az);
-#endif
-
+  Serial.print(sensor_back.calcAccel(sensor_back.az), 3);
 }
 
-void printMag()
-{  
-  // Now we can use the mx, my, and mz variables as we please.
-  // Either print them as raw ADC values, or calculated in Gauss.
-  Serial.print("M: ");
-#ifdef PRINT_CALCULATED
-  // If you want to print calculated values, you can use the
-  // calcMag helper function to convert a raw ADC value to
-  // Gauss. Give the function the value that you want to convert.
-  Serial.print(imu.calcMag(imu.mx), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcMag(imu.my), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcMag(imu.mz), 2);
-  Serial.print(", ");
-  Serial.print(imu2.calcMag(imu2.mx), 2);
-  Serial.print(", ");
-  Serial.print(imu2.calcMag(imu2.my), 2);
-  Serial.print(", ");
-  Serial.print(imu2.calcMag(imu2.mz), 2);
-  Serial.println(" gauss");
-#elif defined PRINT_RAW
-  Serial.print(imu.mx);
-  Serial.print(", ");
-  Serial.print(imu.my);
-  Serial.print(", ");
-  Serial.println(imu.mz);
-#endif
-}
-
-// Calculate pitch, roll, and heading.
-// Pitch/roll calculations take from this app note:
-// http://cache.freescale.com/files/sensors/doc/app_note/AN3461.pdf?fpsp=1
-// Heading calculations taken from this app note:
-// http://www51.honeywell.com/aero/common/documents/myaerospacecatalog-documents/Defense_Brochures-documents/Magnetic__Literature_Application_notes-documents/AN203_Compass_Heading_Using_Magnetometers.pdf
-void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
+void printAttitude(LSM9DS1 sensor)
 {
+  float ax = sensor.ax;
+  float ay = sensor.ay;
+  float az = sensor.az;
+  float mx = sensor.mx;
+  float my = sensor.my;
+  float mz = sensor.mz;
+  
   float roll = atan2(ay, az);
   float pitch = atan2(-ax, sqrt(ay * ay + az * az));
   
@@ -289,9 +158,9 @@ void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
   pitch *= 180.0 / PI;
   roll  *= 180.0 / PI;
   
-  Serial.print("Pitch, Roll: ");
-  Serial.print(pitch, 2);
+  Serial.print(pitch, 3);
   Serial.print(", ");
-  Serial.println(roll, 2);
-  Serial.print("Heading: "); Serial.println(heading, 2);
+  Serial.print(roll, 3);
+  Serial.print(", ");
+  Serial.print(heading, 3);
 }
--- Visualization/visualization/visualization.pde
+++ Visualization/visualization/visualization.pde
@@ -1,125 +1,163 @@
-
+ //<>// //<>//
+//<<<<<<< HEAD
+////hi
+//=======
+//>>>>>>> 97f210a6c9a74105442c787a0315af235d645ee1
 import processing.serial.*;
 import controlP5.*;
+import signal.library.*;
+
+
+// Data class for accelerometer data
+public class AccelMeasurements{
+  float m_x_acc_l, m_y_acc_l, m_z_acc_l, m_roll_l, m_pitch_l, m_yaw_l;
+  
+  public AccelMeasurements(){
+  // constructor for the class, empty for now 
+  }
+  
+  // Setter function for class members, takes accelerometer data and casts to a float 
+  public void ImportData(String x_acc_l, String y_acc_l, String z_acc_l, String roll_l, String pitch_l, String yaw_l){
+    this.m_x_acc_l = float(x_acc_l); 
+    this.m_y_acc_l = float(y_acc_l);
+    this.m_z_acc_l = float(z_acc_l);
+    this.m_roll_l = float(roll_l);
+    this.m_pitch_l = float(pitch_l);
+    this.m_yaw_l = float(yaw_l);
+  }
+}  
 
 Serial myPort;
+AccelMeasurements AccelData_Front, AccelData_Back;
 String val;
-float x_acc_1,y_acc_1,z_acc_1,roll_1,pitch_1,yaw_1;
+//float x_acc_1,y_acc_1,z_acc_1,roll_1,pitch_1,yaw_1;
 PFont f;
 ControlP5 cp5;
+SignalFilter frontFilter, backFilter;
 Chart myChart;
 
-String ButtonPressed;
-Button X1, Y1;
-
+float minCutoff = 0.05;
+float beta      = 4.0;
+ 
 void setup()
 {
   size(1200,600, P3D);
    //cp5 and chart objects
-  /* cp5 = new ControlP5(this);
+   cp5 = new ControlP5(this);
+   frontFilter = new SignalFilter(this);
+   backFilter = new SignalFilter(this);
    myChart = cp5.addChart("data")
                .setPosition(600, 50)
                .setSize(500, 300)
-               .setRange(-20, 20)
+               .setRange(-0.5, 0.5)
                .setView(Chart.LINE) // use Chart.LINE, Chart.PIE, Chart.AREA, Chart.BAR_CENTERED
                .setStrokeWeight(15)
                .setColorCaptionLabel(color(40))
                .setResolution(100)
                ;
-       
+   
   myChart.addDataSet("incoming");
   myChart.setData("incoming", new float[100]);
-  
-  ButtonPressed = "None";
-  // create a new button with name 'X1'
-  X1 = cp5.addButton("X1")
-     .setValue(0)
-     .setPosition(500,50)
-     .setSize(80,15)
-     .setSwitch(true);
-     ;
-  
-    // create a new button with name 'Y1'
-  Y1 = cp5.addButton("Y1")
-     .setValue(0)
-     .setPosition(500,80)
-     .setSize(80,15)
-     .setSwitch(true);
-     ;*/
-  
+
   //setup serial port and communication between arduino and processing
   String portName = Serial.list()[0]; //depends on your serial port
-  myPort = new Serial(this, portName, 9600); 
+  myPort = new Serial(this, portName, 115200); 
+  AccelData_Front = new AccelMeasurements();
+  AccelData_Back = new AccelMeasurements();
   f = createFont("Arial", 16, true);
   
   //make orthogonal view
   ortho(-width/2, width/2, -height/2, height/2);
   
   //Initialize data variables
-  x_acc_1 = 0.0;
-  y_acc_1 = 0.0;
-  z_acc_1 = 0.0;
-  roll_1 = 0.0;
-  pitch_1 = 0.0;
-  yaw_1 = 0.0;
+  //x_acc_1 = 0.0;
+  //y_acc_1 = 0.0;
+  //z_acc_1 = 0.0;
+  //roll_1 = 0.0;
+  //pitch_1 = 0.0;
+  //yaw_1 = 0.0;
+   
   
+  // Disable graph until we want to plot
+  cp5.setAutoDraw(false); 
 }
 
 void draw()
 {
+    frontFilter.setMinCutoff(minCutoff);
+    frontFilter.setBeta(beta);
+    backFilter.setMinCutoff(minCutoff);
+    backFilter.setBeta(beta);
+
     if ( myPort.available() > 0) 
     {  // If data is available,
-      val = myPort.readStringUntil('\n');         // read serial data and store it in val
-      String[] data = splitTokens(val, "\t");  //split up serial data and store in array of strings
-    
-      //cast each string into a float and store into appropriate variables
-      x_acc_1 = float(data[1]);
-      y_acc_1 = float(data[3]);
-      z_acc_1 = float(data[5]);
-      roll_1 = float(data[7]);
-      pitch_1 = float(data[9]);
-      yaw_1 = 0.0;
+      int lf = 10; 
+      val = myPort.readStringUntil(lf);         // read serial data and store it in val
+      String [] data; 
       
-      //Display obtained data on screen at the top
-      background(100); //clear screen
-      textFont(f,15);                  
-      fill(255);                         
-      text("X1:  " + data[1] + "    Y1: " + data[3] + "    Z1: " + data[5] + 
-      "    Roll1: " + data[7] + "    Pitch1: " + data[9],20,20);
+      if (val != null){
+        data = splitTokens(val, ", ");  //split up serial data and store in array of strings
+      }
+      else{
+        data = null; 
+      }
       
-      //display accelerometer as 3D box
-      displayBox(roll_1, pitch_1, yaw_1, 200, 150, 0);
-      
-      //plotChart();
-     
+      if (data != null){
+        if (data.length == 12){
+          //cast each string into a float and store into appropriate variables
+          
+          AccelData_Front.ImportData(data[0],data[1],data[2],data[6],data[7],data[8]);  // Currently just trying to duplicate the rotating box
+          AccelData_Back.ImportData(data[3],data[4],data[5],data[9],data[10],data[11]);
+          
+          AccelData_Front.m_z_acc_l = frontFilter.filterUnitFloat( AccelData_Front.m_z_acc_l );
+          AccelData_Back.m_z_acc_l = backFilter.filterUnitFloat( AccelData_Back.m_z_acc_l );
+          //x_acc_1 = float(data[1]);
+          //y_acc_1 = float(data[3]);
+          //z_acc_1 = float(data[5]);
+          
+          //roll_1 = float(data[7]);
+          //pitch_1 = float(data[9]);
+          //yaw_1 = 0.0;
+          
+          //Display obtained data on screen at the top
+          //TODO: Encapsulate the text printing into a function 
+          background(100); //clear screen
+          textFont(f,15);                  
+          fill(255);                         
+          text("X1:  " + data[0] + "    Y1: " + data[1] + "    Z1: " + AccelData_Front.m_z_acc_l + 
+          "    Roll1: " + data[6] + "    Pitch1: " + data[7],20,20);
+          text("X2:  " + data[3] + "    Y2: " + data[4] + "    Z2: " + AccelData_Back.m_z_acc_l + 
+          "    Roll2: " + data[9] + "    Pitch2: " + data[10],20,50);
+          
+          //display accelerometer as 3D box
+          //Note: The library will treat subsequent translation/rotation data as
+          // an aggregate of the last input until the loop/cycle refreshes
+          displayBox(AccelData_Front, 200, 150, 0, #ff0000);  // Accelerometer 1's visualization object
+          displayBox(AccelData_Back, 200, 300, 0, 51);  // Accelerometer 2's visualization object
+        
+          // Enable chart and plot data
+          camera(); 
+          cp5.draw(); 
+          plotChart();
+        }
+      }    
+       
     } 
 
 }
 
-public void controlEvent(ControlEvent theEvent) {
-  ButtonPressed = theEvent.getController().getName();
+void displayBox (AccelMeasurements AccelData, int xPos, int yPos, int zPos, int rgb){
+      pushMatrix();
+      stroke(255);
+      fill(rgb);
+      translate(xPos, yPos, zPos);
+      rotateX(radians(AccelData.m_roll_l));
+      rotateY(radians(-1*AccelData.m_pitch_l));
+      box(150, 150, 20);  
+      popMatrix();
 }
 
-void displayBox (float roll, float pitch, float yaw, int xPos, int yPos, int zPos){
-      stroke(255);
-      fill(51);
-      translate(xPos, yPos, zPos);
-      rotateX(radians(roll));
-      rotateY(radians(-1*pitch));
-      box(150, 150, 20);  
-}
 
 void plotChart(){
-  if(ButtonPressed == "X1"){
-    myChart.push("incoming", x_acc_1);
-    Y1.setOff();
-  } 
-  
-  if(ButtonPressed == "Y1"){
-    myChart.push("incoming", y_acc_1);
-    X1.setOff();
-  }
-}
-
-
-    
+      myChart.push("incoming", AccelData_Front.m_z_acc_l + AccelData_Back.m_z_acc_l);
+}   

bce0f142583f1f091317d3a51ad4d61ceafd0c58
DanielZhou4970 <danielzhou4970@gmail.com>
2018-09-09 15:47:33 -0700

    Modified base read from serial framework

---
+++ cplusplus_read_from_serial_test/serial_read_test/SerialPort.h
@@ -0,0 +1,41 @@
+// Base serial read header and source files retrieved from https://playground.arduino.cc/Interfacing/CPPWindows
+// Requires the Microsoft .NET Framework to make use of System.IO
+#ifndef SERIALPORT_H_INCLUDED
+#define SERIALPORT_H_INCLUDED
+
+#define ARDUINO_WAIT_TIME 2000
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+class Serial
+{
+private:
+	//Serial comm handler
+	HANDLE hSerial;
+	//Connection status
+	bool connected;
+	//Get various information about the connection
+	COMSTAT status;
+	//Keep track of last error
+	DWORD errors;
+
+public:
+	//Initialize Serial communication with the given COM port
+	Serial(const char *portName);
+	//Close the connection
+	~Serial();
+	//Read data in a buffer, if nbChar is greater than the
+	//maximum number of bytes available, it will return only the
+	//bytes available. The function return -1 when nothing could
+	//be read, the number of bytes actually read.
+	int ReadData(char *buffer, unsigned int nbChar);
+	//Writes data from a buffer through the Serial connection
+	//return true on success.
+	bool WriteData(const char *buffer, unsigned int nbChar);
+	// Status flag for connection to the serial device 
+	bool IsConnected();
+};
+
+#endif // SERIALPORT_H_INCLUDED
---
+++ cplusplus_read_from_serial_test/serial_read_test/main.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <tchar.h>
+#include "SerialPort.h"	
+#include <string.h>
+
+using namespace std;
+
+// application reads from the specified serial port and reports the collected data
+int main(int argc, _TCHAR* argv[]) {
+	Serial Arduino("\\\\.\\COM4");    // Adjust to match name of COM port 
+
+	if (Arduino.IsConnected())
+		printf("We're connected");
+
+	char incomingData[256] = "";			// don't forget to pre-allocate memory
+	//printf("%s\n",incomingData);
+	int dataLength = 255;
+	int readResult = 0;
+
+	while(Arduino.IsConnected())
+	{
+		readResult = Arduino.ReadData(incomingData,dataLength);
+		printf("Bytes read: (0 means no data available) %i\n",readResult);
+		// Arduino data is received as a string, will also need to separate and pack
+		// into individual variables (or a class/struct for each measurement
+        incomingData[readResult] = 0;
+
+		printf("%s",incomingData);
+		Sleep(500);
+	}
+	return 0;
+}

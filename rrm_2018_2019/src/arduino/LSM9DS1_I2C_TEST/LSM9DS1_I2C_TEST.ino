c81a4279167e4a187219edc4158b83b5c59d30d0
zachfu97 <zachfu97@gmail.com>
2018-10-20 12:40:12 -0700

    Changed serial data output format + rate

--- rrm_2018_2019/src/arduino/LSM9DS1_I2C_TEST/LSM9DS1_I2C_TEST.ino
+++ rrm_2018_2019/src/arduino/LSM9DS1_I2C_TEST/LSM9DS1_I2C_TEST.ino
@@ -37,12 +37,16 @@ LSM9DS1 sensor_back;
 ////////////////////////////
 #define PRINT_SPEED 50 // ms between prints
 static unsigned long lastPrint = 0; // Keep track of print time
+static unsigned long milli = 0;
+boolean toggle1 = false;
+boolean printSample = false;
 
 //http://www.magnetic-declination.com/
 #define DECLINATION 16.23 // Declination (degrees) in Vancouver, Canada (16 deg, 14')
 
 void setup() 
 {
+  pinMode(LED_BUILTIN, OUTPUT);
   
   Serial.begin(115200);
   
@@ -56,8 +60,8 @@ void setup()
   sensor_back.settings.device.mAddress = MAG_BACK;
   sensor_back.settings.device.agAddress = ACC_BACK;
   // The above lines will only take effect AFTER calling
-  // sensor_front.begin(), which verifies communication with the sensor_front
-  // and turns it on.
+  // sensor_front.begin() and sensor_back.begin(), which verifies 
+  // communication with the sensor_front and turns it on.
   if (!sensor_front.begin())
   {
     Serial.println("Failed to communicate with front sensor.");
@@ -66,8 +70,7 @@ void setup()
                   "work for an out of the box LSM9DS1 " \
                   "Breakout, but may need to be modified " \
                   "if the board jumpers are.");
-    while (1)
-      ;
+    while (1);
   }
   if (!sensor_back.begin())
   {
@@ -77,41 +80,98 @@ void setup()
                   "work for an out of the box LSM9DS1 " \
                   "Breakout, but may need to be modified " \
                   "if the board jumpers are.");
-    while (1)
-      ;
+    while (1);
   }
+  
+  cli(); // stop interrupts
+
+  //set timer1 interrupt at 100Hz
+  TCCR1A = 0; // set entire TCCR1A register to 0
+  TCCR1B = 0; // same for TCCR1B
+  TCNT1  = 0; //initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 2499; // = (16*10^6) / (50*64) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12 and CS10 bits for 64 prescaler
+  TCCR1B |= (1 << CS01) | (1 << CS00); 
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+  
+  sei(); // allow interrupts
+}
+
+// Timer1 interrupt triggers at 100Hz
+ISR(TIMER1_COMPA_vect){
+  
+  if (toggle1){
+    digitalWrite(LED_BUILTIN,HIGH);
+    toggle1 = false;
+  }
+  else{
+    digitalWrite(LED_BUILTIN,LOW);
+    toggle1 = true;
+  }
+
+  printSample = true;
 }
 
 void loop()
-{
-  if ( sensor_front.accelAvailable() )
-  {
-    sensor_front.readAccel();
-  }
-  if ( sensor_back.accelAvailable() )
-  {
-    sensor_back.readAccel();
-  }
-  if ( sensor_front.magAvailable() )
-  {
-    sensor_front.readMag();
-  }
-  if ( sensor_back.magAvailable() )
-  {
-    sensor_back.readMag();
-  }
-  
-  if ((lastPrint + PRINT_SPEED) < millis())
-  {
+{ 
+  if(printSample) 
+  { 
+    if ( sensor_front.gyroAvailable() )   sensor_front.readGyro();
+    if ( sensor_back.gyroAvailable() )    sensor_back.readGyro();
+    if ( sensor_front.accelAvailable() )  sensor_front.readAccel();
+    if ( sensor_back.accelAvailable() )   sensor_back.readAccel();
+    if ( sensor_front.magAvailable() )    sensor_front.readMag();
+    if ( sensor_back.magAvailable() )     sensor_back.readMag();
+    
+    printGyro();
+    Serial.print(", ");
     printAccel();
     Serial.print(", ");
-    printAttitude(sensor_front);
+    printMag();
+    Serial.println();
+
+    printSample = false;
+  }
+  /*
+  milli = millis();
+  if ((lastPrint + PRINT_SPEED) <= milli)
+  {
+    if ( sensor_front.gyroAvailable() )   sensor_front.readGyro();
+    if ( sensor_back.gyroAvailable() )    sensor_back.readGyro();
+    if ( sensor_front.accelAvailable() )  sensor_front.readAccel();
+    if ( sensor_back.accelAvailable() )   sensor_back.readAccel();
+    if ( sensor_front.magAvailable() )    sensor_front.readMag();
+    if ( sensor_back.magAvailable() )     sensor_back.readMag();
+    
+    printGyro();
     Serial.print(", ");
-    printAttitude(sensor_back);
+    printAccel();
+    Serial.print(", ");
+    printMag();
     Serial.println();
     
-    lastPrint = millis();
+    lastPrint = milli;
   }
+  */
+}
+
+void printGyro()
+{
+  Serial.print(sensor_front.calcGyro(sensor_front.gx), 3);
+  Serial.print(", ");
+  Serial.print(sensor_front.calcGyro(sensor_front.gy), 3);
+  Serial.print(", ");
+  Serial.print(sensor_front.calcGyro(sensor_front.gz), 3);
+  Serial.print(", ");
+  Serial.print(sensor_back.calcGyro(sensor_back.gx), 3);
+  Serial.print(", ");
+  Serial.print(sensor_back.calcGyro(sensor_back.gy), 3);
+  Serial.print(", ");
+  Serial.print(sensor_back.calcGyro(sensor_back.gz), 3);
 }
 
 void printAccel()
@@ -129,6 +189,21 @@ void printAccel()
   Serial.print(sensor_back.calcAccel(sensor_back.az), 3);
 }
 
+void printMag()
+{ 
+  Serial.print(sensor_front.calcMag(sensor_front.mx), 3);
+  Serial.print(", ");
+  Serial.print(sensor_front.calcMag(sensor_front.my), 3);
+  Serial.print(", ");
+  Serial.print(sensor_front.calcMag(sensor_front.mz), 3);
+  Serial.print(", ");
+  Serial.print(sensor_back.calcMag(sensor_back.mx), 3);
+  Serial.print(", ");
+  Serial.print(sensor_back.calcMag(sensor_back.my), 3);
+  Serial.print(", ");
+  Serial.print(sensor_back.calcMag(sensor_back.mz), 3);
+}
+/*
 void printAttitude(LSM9DS1 sensor)
 {
   float ax = sensor.ax;
@@ -164,3 +239,4 @@ void printAttitude(LSM9DS1 sensor)
   Serial.print(", ");
   Serial.print(heading, 3);
 }
+*/
